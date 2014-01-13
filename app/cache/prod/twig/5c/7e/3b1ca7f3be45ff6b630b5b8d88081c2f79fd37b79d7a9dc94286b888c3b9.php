d3b828ba16c1d28ad4b65b30f0a9e322d4030dea
martins-martins <listentogreatmusic@gmail.com>
2014-01-13 16:49:04 +0200

    Signed-off-by: martins-martins <listentogreatmusic@gmail.com>

---
+++ app/cache/prod/twig/5c/7e/3b1ca7f3be45ff6b630b5b8d88081c2f79fd37b79d7a9dc94286b888c3b9.php
@@ -0,0 +1,51 @@
+<?php
+
+/* AcmeHelloBundle:Hello:index.html.twig */
+class __TwigTemplate_5c7e3b1ca7f3be45ff6b630b5b8d88081c2f79fd37b79d7a9dc94286b888c3b9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeHelloBundle:Hello:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
